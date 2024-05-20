19a1e49a4a0dbc7c5d5dae95d6fb6af6d866b041
Wenchang Gao <wenchang.gao@tufts.edu>
2024-05-20 16:19:00 -0400

    working on grasps

--- test_grasp.py
+++ test_grasp.py
@@ -5,6 +5,7 @@ x = gpd_get_grasp_pose('test.pcd',cfg_file_name='test.cfg')
 # print('-------------------------------\n\n\n\n\n\n')
 # print(type(x.pos_x))
 
+print(x.pos_x[0], x.pos_y[0], x.pos_z[0])
 # grasp_pose_path = "/home/wenchang/projects/ForceRL/MjJacoDoorGrasps"
 
 # with open(grasp_pose_path, 'rb') as f:
