pointcloud and affordance rotation bug

env/robot_revolute_env.py
@@ -51,8 +51,10 @@ class RobotRevoluteOpening(SingleArmEnv):
         agentview_camera_quat=[0.6380177736282349, 0.3048497438430786, 0.30484986305236816, 0.6380177736282349], 
 
         # Object rotation
-        obj_rotation=(np.pi, np.pi), 
-        move_robot_away=True
+        obj_rotation=(-np.pi/2, -np.pi / 2),
+        x_range = (-1,-1),
+        y_range = (0,0),
+        move_robot_away=True,
     ):
         self.placement_initializer = placement_initializer
         self.table_full_size = (0.8, 0.3, 0.05)
@@ -61,6 +63,8 @@ class RobotRevoluteOpening(SingleArmEnv):
         self.object_type = object_type
         self.object_model_idx = object_model_idx 
         self.obj_rotation = obj_rotation 
+        self.x_range = x_range
+        self.y_range = y_range
         self.move_robot_away = move_robot_away
 
         super().__init__(
@@ -133,10 +137,13 @@ class RobotRevoluteOpening(SingleArmEnv):
         super()._load_model()
 
         # set robot position
-        xpos = self.robots[0].robot_model.base_xpos_offset["table"](self.table_full_size[0]) 
-        self.robot_init_xpos = xpos
+        # xpos = self.robots[0].robot_model.base_xpos_offset["table"](self.table_full_size[0]) 
+        xpos = self.robots[0].robot_model.base_xpos_offset["table"](0)
+        # self.robot_init_xpos = xpos
         if self.move_robot_away:
             xpos = (20, xpos[1], xpos[2]) # Move the robot away
+            self.robots[0].robot_model.set_base_xpos(xpos)
+        else:
             self.robots[0].robot_model.set_base_xpos(xpos)
 
         # set empty arena
@@ -151,7 +158,7 @@ class RobotRevoluteOpening(SingleArmEnv):
 
         # get the revolute object
         if self.object_type == "microwave":
-            self.revolute_object = SelectedMicrowaveObject(name="drawer", microwave_number=self.object_model_idx, scale=False)
+            self.revolute_object = SelectedMicrowaveObject(name="microwave", microwave_number=self.object_model_idx, scale=False)
 
         # Create placement initializer
         if self.placement_initializer is not None:
@@ -175,10 +182,12 @@ class RobotRevoluteOpening(SingleArmEnv):
             self.placement_initializer = UniformRandomSampler(
                 name="ObjectSampler",
                 mujoco_objects=self.revolute_object,
-                x_range=[1, 1], #No randomization
-                y_range=[1, 1], #No randomization 
+                x_range = self.x_range, #No randomization
+                y_range = self.y_range, #No randomization 
                 z_offset=0.1,
                 # x_range=[0, 0], #No randomization
+                # x_range=[-1, -1], #No randomization
+                # y_range=[-0.54,-0.54], #No randomization
                 # y_range=[0,0], #No randomization
                 rotation=self.obj_rotation, #No randomization
                 rotation_axis="z",

experiments/robot_grasp_microwave.py
@@ -12,10 +12,13 @@ import robosuite.utils.transform_utils as transform
 from utils.control_utils import PathPlanner, Linear, Gaussian
 import os 
 import json
-from scipy.spatial.transform import Rotation as R
+from scipy.spatial.transform import Rotation as R 
+import utils.sim_utils as sim_utils
+from grasps.aograsp.get_pointclouds import get_aograsp_ply_and_config
+from grasps.aograsp.get_affordance import get_affordance_main
 
 
-object_name = "microwave_closed" # A microwave called "drawer"!!!
+object_name = "temp_door" # A microwave called "drawer"!!!
 
 controller_name = "OSC_POSE"
 controller_cfg_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),"controller_configs")
@@ -25,9 +28,11 @@ controller_configs = suite.load_controller_config(custom_fpath = controller_cfg_
 with open(controller_cfg_path, 'r') as f:
     controller_configs = json.load(f)
 # print(controller_configs)
-env:RobotDrawerOpening = suite.make(
-    "RobotRevoluteOpening",
+
+env_kwargs = dict(
     robots="Panda",
+    object_type = "microwave",
+    obj_rotation=(np.pi*2/3, np.pi*2/3),
     has_renderer=True,
     use_camera_obs=True,
     has_offscreen_renderer=True,
@@ -37,19 +42,59 @@ env:RobotDrawerOpening = suite.make(
     control_freq = 20,
     horizon=10000,
     camera_names = ["birdview", "agentview", "frontview", "sideview"],
+    # render_camera = "birdview",
     camera_heights = [1024,256,512,1024],
-    camera_widths = [1024,1024,1024,1024]
+    camera_widths = [1024,1024,1024,1024],
+    move_robot_away = False,
+    x_range = (-1,-1),
+    y_range = (0,0),
+)
+env_name = "RobotRevoluteOpening"
+
+env:RobotDrawerOpening = suite.make(
+    env_name,
+    **env_kwargs
 )
 
 obs = env.reset()
+
+sim_utils.init_camera_pose(env, camera_pos=np.array([-0.77542536, -0.02539806,  0.30146208]), scale_factor=3) 
+
+pcd_wf_path, camera_info_path = get_aograsp_ply_and_config(env_name = env_name, 
+                        env_kwargs=env_kwargs,
+                        object_name=env_kwargs["object_type"], 
+                        camera_pos=np.array([-0.77542536, -0.02539806,  0.30146208]),
+                        camera_quat=np.array([-0.005696068282031459, 0.19181093598117868, 0.02913152799094475, 0.9809829120433564]),
+                        scale_factor=3,
+                        pcd_wf_path='point_clouds/world_frame_pointclouds/world_frame_temp_door.ply',
+                        viz=True
+                        )
+
+print(pcd_wf_path)
+print(camera_info_path)
+
+get_affordance_main(pcd_wf_path, camera_info_path)
+
 # print(type(obs["gripper_quat"]))
 # env.render()
 camera_name = "frontview"
 depth_image = obs['{}_depth'.format(camera_name)]
 depth_image = flip_image(depth_image)
 
-proposals = np.load(f'outputs/grasp_proposals/{object_name}.npz', allow_pickle=True)['data'].item()
+proposals = np.load(f'outputs/grasp_proposals/world_frame_proposals/world_frame_{object_name}_grasp.npz', allow_pickle=True)
 print(proposals.keys())
+
+proposal_pos = proposals['pos'] # np.array(np.array(3), ...)
+proposal_quat = proposals['quat'] # np.array(np.array(4), ...)
+proposal_scores = proposals['scores'] # np.array(np.float32, ...)
+
+print(type(proposal_pos[0])) 
+print(type(proposal_scores[0])) 
+
+for i in range(30000):
+    action = np.zeros_like(env.action_spec[0])
+    env.step(action)
+    env.render()
 
 hand_pose = np.array([-0.2030102014541626, -0.540092134475708, 1.2524129152297974]) 
 # Robosuite have an x pointing out of screen to you and an Z facing upward 
@@ -58,9 +103,11 @@ rotation_euler = np.array([0.005762052722275257, -0.984636664390564,0.1745204925
 # rotation_euler = np.array([0,0,0])
 rotation_matrix = transform.euler2mat(rotation_euler)
 
-rotation_matrix = np.array([[-0.0073337797075510025, -0.5499632358551025,0.8351566791534424],
+rotation_matrix = np.array([
+    [-0.0073337797075510025, -0.5499632358551025,0.8351566791534424],
     [0.0029766582883894444, -0.8351874351501465,-0.5499573349952698],          
-    [0.9999686479568481, -0.001547289895825088,0.00776213314384222]])
+    [0.9999686479568481, -0.001547289895825088,0.00776213314384222]
+    ])
 rotation_matrix = np.array([[-0.10221315920352936, -0.9264970421791077,-0.3621542751789093],
                             [-0.3834446370601654, 0.37262317538261414,-0.845057487487793],
                             [0.9178903102874756, 0.05249011516571045,-0.3933473229408264]])
@@ -84,5 +131,6 @@ K_epochs = 4
 eps_clip = 0.2 
 action_std = 0.5 
 has_continuous_action_space = True 
+env.close()
 
 

grasps/aograsp/get_affordance.py
@@ -49,7 +49,7 @@ def get_aograsp_pts_in_cam_frame_z_front_with_info(pts_wf, info_path):
 
     return pts_cf 
 
-def inference_affordance(model, pcd_wf_path, camera_info_path, device, args): 
+def inference_affordance(model, pcd_wf_path, camera_info_path, device): 
     '''
     Converts a world frame point cloud to camera frame and saves it under "point_clouds/camera_frame_pointclouds/camera_frame_$(object_name).ply", then
     inference the affordance of a point cloud using the AO-Grasp model. The camera frame affordance is saved in "outputs/point_score/camera_frame_$(object_name)_affordance.npz" and the heatmap image is saved in "outputs/point_score_img/heatmap_$(object_name).png"
@@ -125,9 +125,27 @@ def inference_affordance(model, pcd_wf_path, camera_info_path, device, args):
     print('Saved heatmap to ', 'outputs/point_score/'+'camera_frame_'+data_name+'_affordance.npz')
     return test_dict, pts_arr
 
+def get_affordance_main(pcd_wf_path, camera_info_path, device="cuda:0"):
+    '''
+    main function for getting affordance
+    '''
+
+    # load ao-grasp model
+    model = m_utils.load_model(
+        model_conf_path='checkpoints/grasp_models/aograsp_models/conf.pth', 
+        ckpt_path='checkpoints/grasp_models/aograsp_models/770-network.pth'
+    )
+    model.to(device)
+
+    # do the inference
+    t_dict, pts = inference_affordance(model, pcd_wf_path, camera_info_path, device) 
+    visualize_heatmap(t_dict, pts, point_score_dir = None, point_score_img_dir = None, data_name = "1")
+
+
+
 def visualize_heatmap(test_dict, pts, point_score_dir, point_score_img_dir, data_name):
     scores = test_dict["point_score_heatmap"][0].cpu().numpy()
-    pcd_path = os.path.join(point_score_dir, f"{data_name}.npz")
+    # pcd_path = os.path.join(point_score_dir, f"{data_name}.npz")
 
     pts_arr = pts
     mean = np.mean(pts_arr, axis=0) 
@@ -140,8 +158,14 @@ def visualize_heatmap(test_dict, pts, point_score_dir, point_score_img_dir, data
     # print(pcd_path)
 
     # Save image of heatmap
-    fig_path = os.path.join(point_score_img_dir, f"heatmap_{data_name}.png")
-    hist_path = os.path.join(point_score_img_dir, f"heatmap_{data_name}_hist.png") 
+    if point_score_img_dir:
+        fig_path = os.path.join(point_score_img_dir, f"heatmap_{data_name}.png")
+    else:
+        fig_path = None
+    if point_score_img_dir:
+        hist_path = os.path.join(point_score_img_dir, f"heatmap_{data_name}_hist.png") 
+    else: 
+        fig_path = None
     try:
         v_utils.viz_heatmap(
             heatmap_dict["pts"],
@@ -158,7 +182,7 @@ def visualize_heatmap(test_dict, pts, point_score_dir, point_score_img_dir, data
         save_path=hist_path,
         scale_cmap_to_heatmap_range=True,
     )
-    print(f"Heatmap saved to: {pcd_path}")
+    # print(f"Heatmap saved to: {pcd_path}")
     print(f"Visualization saved to: {fig_path}")
 
 
@@ -211,6 +235,6 @@ if __name__ == "__main__":
     pcd_wf_path = args.pcd_wf_path 
     camera_info_path = args.camera_info_path
     device = args.device
-    t_dict, pts = inference_affordance(model, pcd_wf_path, camera_info_path, device, args) 
+    t_dict, pts = inference_affordance(model, pcd_wf_path, camera_info_path, device) 
     # pts = o3d.io.read_point_cloud(pcd_path) 
     visualize_heatmap(t_dict, pts, '/home/wgao22/projects/ForceRL/outputs/point_score', '/home/wgao22/projects/ForceRL/outputs/point_score_img', 'microwave')