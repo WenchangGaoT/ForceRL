3cf903b118cda348e92dbe7bae58ad651781b4a7
WenchangGaoT <wenchang.gao@tufts.edu>
2024-08-25 21:53:22 -0400

    added eval revolute obj

--- objects/custom_objects.py
+++ objects/custom_objects.py
@@ -253,6 +253,65 @@ class EvalPrismaticObjects(MujocoXMLObject):
         joint_range = joint.get("range")
         joint_range = joint_range.split(" ")
         joint_range = [float(x) for x in joint_range]
+        return joint_range 
+    
+
+class EvalRevoluteObjects(MujocoXMLObject):
+    def __init__(self, name, scaled = True, scale = 1.0):
+        self.available_obj_list = self.available_objects()
+        assert name in self.available_obj_list, "Invalid object! Please use another name"
+
+        if not scaled:
+            xml_path = f"{name}/{name}.xml"
+        
+        else:
+            # call function to scale the object
+            xml_path_original = fill_custom_xml_path(f"{name}/{name}.xml")
+            scale_object_new(xml_path_original, None, [scale, scale, scale])
+            xml_path = f"{name}/{name}-scaled.xml"
+        super().__init__(
+            fill_custom_xml_path(xml_path), name=name, joints=None, obj_type="all",duplicate_collision_geoms=True
+            ) 
+        self.revolute_body = self.naming_prefix + "link_0"
+        self.hinge_joint = self.naming_prefix + "joint_0"
+    
+    @property
+    def hinge_pos_relative(self):
+        '''
+        Returns:
+            str: hinge position relative to the object
+        '''
+        hinge = find_elements(root=self.worldbody, tags="joint", attribs={"name": self.hinge_joint}, return_first=True)
+        hinge_pos = hinge.get("pos")
+        hinge_pos = hinge_pos.split(" ")
+        hinge_pos = [float(x) for x in hinge_pos]
+        return hinge_pos
+
+    @staticmethod
+    def available_objects():
+        available_objects = [
+            # 'microwave-1', 
+            'microwave-2', 
+            'microwave-3',
+            'microwave-4',
+            'microwave-5', 
+
+            # TODO: TRAIN DISHWASHER POLICIES
+            # 'dishwasher-2', 
+            # 'dishwasher-3'
+        ]
+        return available_objects
+    
+    @property
+    def joint_range(self):
+        '''
+        Returns:
+            str: hinge ransge
+        '''
+        joint = find_elements(root=self.worldbody, tags="joint", attribs={"name": self.joint}, return_first=True)
+        joint_range = joint.get("range")
+        joint_range = joint_range.split(" ")
+        joint_range = [float(x) for x in joint_range]
         return joint_range
 
 
@@ -284,8 +343,17 @@ class TrainRevoluteObjects(MujocoXMLObject):
             self.lock = False
             # set the door friction and damping to 0 for training
         self._set_door_friction(0.0)
-        self._set_door_damping(0.0)
-
+        self._set_door_damping(0.0) 
+    
+    @staticmethod
+    def available_objects():
+        available_objects = [
+            'train-drawer-1', 
+            # 'cabinet-1', 
+            # 'cabinet-2', 
+            # 'cabinet-3', 
+        ]
+        return available_objects
 
     def _set_door_friction(self, friction):
       
@@ -357,7 +425,10 @@ class TrainPrismaticObjects(MujocoXMLObject):
     @staticmethod
     def available_objects():
         available_objects = [
-            'train-drawer-1',
+            'train-drawer-1', 
+            # 'cabinet-1', 
+            # 'cabinet-2', 
+            # 'cabinet-3', 
         ]
         return available_objects
     
